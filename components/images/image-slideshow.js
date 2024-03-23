b938a0cc651fb1c5a2380b0cefeead9fc5c7dae5
RyanYoung72443 <sryoung72443@gmail.com>
2024-03-23 16:58:23 -0400

    setup basic meals structure

--- components/images/image-slideshow.js
+++ components/images/image-slideshow.js
@@ -5,6 +5,8 @@ import Image from "next/image";
 
 import burgerImg from "@/assets/burger.jpg";
 import curryImg from "@/assets/curry.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
 import pizzaImg from "@/assets/pizza.jpg";
 import schnitzelImg from "@/assets/schnitzel.jpg";
 import tomatoSaladImg from "@/assets/tomato-salad.jpg";
@@ -13,6 +15,8 @@ import classes from "./image-slideshow.module.css";
 const images = [
   { image: burgerImg, alt: "A delicious, juicy burger" },
   { image: curryImg, alt: "A delicious, spicy curry" },
+  { image: dumplingsImg, alt: "Steamed dumplings" },
+  { image: macncheeseImg, alt: "Mac and cheese" },
   { image: pizzaImg, alt: "A delicious pizza" },
   { image: schnitzelImg, alt: "A delicious schnitzel" },
   { image: tomatoSaladImg, alt: "A delicious tomato salad" },
